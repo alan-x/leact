f90add0d28e7fedd1f381e95a0790bef01c9a2bc
FollowWinter <lyxxxx@yeah.net>
2018-10-11 14:16:48 +0800

    [更新] 修改项目结果, 打包结果

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -2,17 +2,24 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'index.js'),
-    mode: 'production',
+    entry: path.resolve(__dirname, 'todo.js'),
+    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        libraryTarget: 'umd',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        open: true
     },
     module: {
         rules: [{
             test: /\.js$/,
             loader: "babel-loader"
         }]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "index.html")
+        })
+    ]
 }
---
+++ index.js
@@ -0,0 +1,9 @@
+import Leact from "./src/Leact";
+import LeactDom from "./src/LeactDom";
+import Component from "./src/Component";
+
+export {
+    LeactDom,
+    Component
+}
+export default Leact
